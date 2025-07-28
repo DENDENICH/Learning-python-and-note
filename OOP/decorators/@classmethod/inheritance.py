2e40ac076c887e92d7c7f6b7430fd55904143faa
DENDENICH <daniil.gol26@yandex.ru>
2025-07-28 10:30:33 +0300

    inheritance

---
+++ OOP/decorators/@classmethod/inheritance.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, field
+
+
+class Parent:
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(**data)
+
+
+class Child(Parent):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+
+
+@dataclass
+class ParentData:
+    _model: int = field(default=1)
+
+
+
+
+c = Child.from_dict({"name": "John", "age": 20})
+print(c.__dict__) # {'name': 'John', 'age': 20}
